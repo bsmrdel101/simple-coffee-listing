e2b0fee4bdce691226292a84ee9a46c9e645df9a
Bennett Smrdel <bennett@midwestdiesel.com>
2024-09-12 11:38:24 -0500

    feat: Handle sold out and no ratings

--- src/components/shop-panel/ShopPanelProduct.tsx
+++ src/components/shop-panel/ShopPanelProduct.tsx
@@ -14,9 +14,21 @@ export default function ShopPanelProduct({ product }: Props) {
         <h3>{ product.name }</h3>
         <p className="shop-panel-product__price">{ product.price }</p>
       </div>
-      <div className="shop-panel-product__rating">
-        <Image src="/images/icons/star-filled.svg" alt="Star" width={23} height={23} />
-        <p>{ product.rating } <span>({ product.votes } votes)</span></p>
+      <div className="shop-panel-product__rating-container">
+        <div className="shop-panel-product__rating">
+          {product.rating > 0 ?
+            <>
+              <Image src="/images/icons/star-filled.svg" alt="Star" width={23} height={23} />
+              <p>{ product.rating } <span>({ product.votes } votes)</span></p>
+            </>
+          :
+            <>
+              <Image src="/images/icons/star-empty.svg" alt="Empty star" width={23} height={23} />
+              <p><span>No ratings</span></p>
+            </>
+          }
+        </div>
+        { !product.available && <p className="shop-panel-product__sold-out">Sold out</p> }
       </div>
     </div>
   );
