8e69932fe00b333830d52ee89d9efd60bce5f1cb
Bennett Smrdel <bennett@midwestdiesel.com>
2024-09-12 15:36:14 -0500

    style: Add scribble

--- src/components/shop-panel/ShopPanelHeader.tsx
+++ src/components/shop-panel/ShopPanelHeader.tsx
@@ -1,6 +1,10 @@
+import Image from "next/image"
+
+
 export default function ShopPanelHeader() {
   return (
     <header>
+      <Image src="/images/scribble.svg" alt="Coffee scribble" width={300} height={300} />
       <h1>Our Collection</h1>
       <p>Introducing our Coffee Collection, a selection of unique coffees from different roast types and origins, expertly roasted in small batches and shipped fresh weekly.</p>
     </header>
